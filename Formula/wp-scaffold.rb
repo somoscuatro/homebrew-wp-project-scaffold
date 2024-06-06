be5c054eae426e17ced6b4423ba611b6fb4161e2
Luca Pipolo <luca@somoscuatro.es>
2024-06-06 15:18:32 +0200

    refactor: rename script

--- Formula/wp-scaffold.rb
+++ Formula/wp-scaffold.rb
@@ -1,12 +1,12 @@
-class WpScaffold < Formula
+class WpProjectScaffold < Formula
   desc "Scaffolds a WordPress project using Docker and optionally a starter theme"
-  homepage "https://github.com/somoscuatro/homebrew-wp-scaffold"
-  url "https://github.com/somoscuatro/homebrew-wp-scaffold/archive/refs/tags/v0.6.0.tar.gz"
+  homepage "https://github.com/somoscuatro/homebrew-wp-project-scaffold"
+  url "https://github.com/somoscuatro/homebrew-wp-project-scaffold/archive/refs/tags/v0.6.0.tar.gz"
   sha256 "c8094f59fcbca24ee2c77a4381352999131e9b1ec6b5233587b06055ca1b4a4e"
   license "MIT"
 
   def install
-    bin.install "wp-scaffold.sh"
+    bin.install "wp-project-scaffold.sh" => "wp-project-scaffold"
   end
 
   test do

e0982ba2fb2126b5777ac011bf8749b400b0050a
Luca Pipolo <luca@somoscuatro.es>
2024-06-06 15:06:47 +0200

    chore: update formula sha256

--- Formula/wp-scaffold.rb
+++ Formula/wp-scaffold.rb
@@ -2,7 +2,7 @@ class WpScaffold < Formula
   desc "Scaffolds a WordPress project using Docker and optionally a starter theme"
   homepage "https://github.com/somoscuatro/homebrew-wp-scaffold"
   url "https://github.com/somoscuatro/homebrew-wp-scaffold/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "abcfc0d0c66f4ab641a983db73f9093580f17a775827b77518589a8b1a42a938"
+  sha256 "eff346ed4dae2bbbab9b538ff0c269afe76cae8d3f2ebe6d49ca0eb8b3983521"
   license "MIT"
 
   def install
